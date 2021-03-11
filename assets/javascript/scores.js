a048359f3283b86e5749bdaf6222ca8e5320ff49
Carol Nguyen <cthoa01@gmail.com>
2021-03-11 00:31:57 -0800

    updated score

--- assets/javascript/scores.js
+++ assets/javascript/scores.js
@@ -1,58 +1,27 @@
-// Setting high score list as variables for manipulation 
+var restartBtn = document.querySelector("button.restartBtn"),
+  clearBtn = document.querySelector("button.clearBtn"),
+  // get the highScores list and turn it back into an object
+  highScores = JSON.parse(localStorage.getItem("highScores") || "[]"),
+  scoreList = document.getElementById("score-list");
 
-var scoreList = document.querySelector("#highscore-list");
+// sort scores from high to low
+highScores.sort(function (a, b) {
+  return b.score - a.score
+})
 
-var highscores = {
-  initials : [],
-  score: [],
+// display the scores
+for (var s = 0; s < highScores.length; s++) {
+  var newLi = document.createElement("li")
+  newLi.textContent = highScores[s].name + " - " + highScores[s].score
+  scoreList.appendChild(newLi)
 }
 
-function getScores() {
-  var storedHighscoresString = localStorage.getItem("highscores");
 
-  if (storedHighscoresString !== null) {
-    // Turning the high score string into a list
-    var storedHighscores = JSON.parse(storedHighscoresString);
-    highscores.initials = storedHighscores.initials;
-    highscores.scores = storedHighscores.scores;
-  }
-  else {
-    highscores.initials = [];
-    highscores.scores = [];
-  }
-}
-
-function renderScores() {
-  scoreList.innerHTML = "";
-
-  getScores();
-
-  for (var i = 0; i < highscores.initials.length; i++) {
-    var list 
-    var 
-    p 
-    p 
-
-    list 
-    scoreList
-  }
-}
-  function saveScore (newInitials, newScore) {
-    getScores();
-
-    highscores.initials.push(newInitials);
-    highscores.scores.push(newScore);
-
-    var storedHighscoresString = JSON.stringify(highscores);
-    localStorage.setItem("highscores", highscoresString);
-  }
-
-  // This function clears the local storage of the high scores including the initials and scores.
-function clearScores() {
-  localStorage.removeItem("highscores");
-  renderScores();
-}
-
-if (scoreList !== null) {
-  renderScores();
-}
+// click handlers for restart and clearing scoreboard
+clearBtn.addEventListener("click", function () {
+  localStorage.clear();
+  history.back()
+});
+restartBtn.addEventListener("click", function () {
+  history.back();
+});
